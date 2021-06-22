3079667d39c1385cb4af6ca1fc1328fe95fe29dc
Vladyslav Siminko <siminko.vlad23@gmail.com>
2021-06-22 12:56:38 +0300

    add loader

---
+++ src/components/Loader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import '../styles/loader.scss';
+
+export const Loader = () => (
+    <div className="lds-ring">
+        <div />
+        <div />
+        <div />
+        <div />
+    </div>
+);
